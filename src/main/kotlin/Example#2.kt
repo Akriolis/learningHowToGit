8ca8ee1069963432f3f448015b7fb39b60c12609
Akriolis <Akriolis@gmail.com>
2022-08-26 16:19:44 +0400

    Added few lines of practice code #2

---
+++ src/main/kotlin/Example#2.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var x = 4
+    while (x > 3){
+        println(x)
+        x = x - 1
+        println(x)
+    }
+    var z = 5
+    while (z == 10){
+        println(z)
+    }
+
+}
