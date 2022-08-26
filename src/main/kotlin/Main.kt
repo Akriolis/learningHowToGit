82517d813429642dbc60eeb3081e44ce4c58216c
Akriolis <Akriolis@gmail.com>
2022-08-26 16:04:11 +0400

    Added few lines of practice code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
 fun main(){
-    println ("Hello world")
-    println ("!")
+    var x = 3
+    val name = "Cormoran"
+    x = x * 10
+    println ("x is $x")
+    while (x > 20) {
+        x = x - 1
+        println ("x is now $x.")
+    }
 
+    for (i in 1..10){
+        x = x + 1
+        println (" x is now $x.")
+    }
+
+    if (x == 20) {
+        println(" x must be $x.")
+    }else{
+        println(" x isn't 20.")
+    }
+    if (name.equals("Cormoran")){
+        println ("$name Strike")
+    }
 }
